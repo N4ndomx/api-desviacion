70e1d80817710bf72577f7b2f7f6d634aed62a04
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2023-10-20 00:40:27 -0600

    update index.js

--- index.js
+++ index.js
@@ -20,6 +20,23 @@ app.get('/proxy-size', (req, res, next) => {
 	res.sendfile("assets/1a_proxy_size.json");
 });
 
+app.get('/3a-test1', (req, res, next) => {
+        res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/3a-test2', (req, res, next) => {
+        res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/3a-test3', (req, res, next) => {
+        res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/3a-test4', (req, res, next) => {
+        res.sendfile("assets/3a_test4.json");
+});
+
+
 // app.get('/header', (req, res, next) => {
 //   res.sendfile("assets/header.json");
 // });
